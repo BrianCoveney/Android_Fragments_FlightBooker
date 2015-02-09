e29338e946d82cfcd4e483245bc8e6991e74d762
Brian Coveney <coveneybrian@gmail.com>
2015-02-09 16:06:10 +0000

    Added a Dialog for the user to select 'Send' or 'Cancel' not quite working right On first run button shows the dialog, then when button is pressed again it launches the email.

--- app/src/main/java/com/example/brian/android_fragments_lt4/view/FlightViewActivity.java
+++ app/src/main/java/com/example/brian/android_fragments_lt4/view/FlightViewActivity.java
@@ -1,11 +1,14 @@
 package com.example.brian.android_fragments_lt4.view;
 
-import android.app.Activity;
+
+import android.app.DialogFragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 import com.example.brian.android_fragments_lt4.R;
 import com.example.brian.android_fragments_lt4.model.Flight;
 
@@ -16,7 +19,7 @@ import java.io.InputStream;
 /**
  * Created by Brian on 13/01/2015.
  */
-public class FlightViewActivity extends Activity {
+public class FlightViewActivity extends FragmentActivity implements SendEmailDialog.SendEmailDialogListener {
 
     private Flight theFlight;
     private Button sendEmail;
@@ -24,36 +27,30 @@ public class FlightViewActivity extends Activity {
     private TextView fileTv;
 
 
+    public void showNoticeDialog() {
+        // Create an instance of the dialog fragment and show it
+        DialogFragment dialog = new SendEmailDialog();
+        dialog.show(getFragmentManager(), "NoticeDialogFragment");
+    }
+
+    // The dialog fragment receives a reference to this Activity through the
+    // Fragment.onAttach() callback, which it uses to call the following methods
+    // defined by the NoticeDialogFragment.NoticeDialogListener interface
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_flightview);
+    public void onDialogPositiveClick(DialogFragment dialog) {
+        // User touched the dialog's positive button
 
 
-        //Button to display text from file in directory res/raw
-        fileBtn = (Button)findViewById(R.id.fileButton);
-        fileBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Read from file into a text view
-                fileTv = (TextView)findViewById(R.id.fileOriginsTv);
-                fileTv.setText(readText());
-            }
-        });
-
-        //Get users flight selection and place into a text view
-        theFlight = (Flight)getIntent().getSerializableExtra("selectedFlight");
-        TextView textView = (TextView)findViewById(R.id.flightDisplayField);
-        textView.setText(theFlight.toString());
-
-        //Email functionality added
-        sendEmail = (Button)findViewById(R.id.emailButton);
+        /***
+         On first run this button shows the dialog, then when button is
+         pressed again it launches the email
+         */
+        sendEmail = (Button) findViewById(R.id.emailButton);
         sendEmail.setOnClickListener(new View.OnClickListener() {
 
 
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_SEND);
                 intent.setType("text/plain");
 
@@ -69,29 +66,72 @@ public class FlightViewActivity extends Activity {
 
                 intent.putExtra(Intent.EXTRA_TEXT, message);
 
-
-                if(intent.resolveActivity(getPackageManager()) != null){
+                if (intent.resolveActivity(getPackageManager()) != null) {
                     startActivity(intent);
                 }
-
             }
         });
     }
+
+    @Override
+    public void onDialogNegativeClick(DialogFragment dialog) {
+        // User touched the dialog's negative button
+        dialog.dismiss();
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flightview);
+
+        //Button to display text from file in directory res/raw
+        fileBtn = (Button) findViewById(R.id.fileButton);
+        fileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Read from file into a text view
+                fileTv = (TextView) findViewById(R.id.fileOriginsTv);
+                fileTv.setText(readText());
+            }
+        });
+
+        //Get users flight selection and place into a text view
+        theFlight = (Flight) getIntent().getSerializableExtra("selectedFlight");
+        TextView textView = (TextView) findViewById(R.id.flightDisplayField);
+        textView.setText(theFlight.toString());
+
+
+        /***
+         On first run this button shows the dialog, then when button is
+         pressed again it launches the email
+         */
+        sendEmail = (Button) findViewById(R.id.emailButton);
+        sendEmail.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                showNoticeDialog();
+            }
+        });
+    }
+
+
     //Function to read text from a file
-    public String readText(){
+    public String readText() {
 
         InputStream inputStream = getResources().openRawResource(R.raw.flight_list);
-        ByteArrayOutputStream byteArrayOutputStream =new ByteArrayOutputStream();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 
         int i;
-        try{
+        try {
             i = inputStream.read();
-            while(i != -1){
+            while (i != -1) {
                 byteArrayOutputStream.write(i);
                 i = inputStream.read();
             }
             inputStream.close();
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return byteArrayOutputStream.toString();
